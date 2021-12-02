2ebc31b5023428475171b6b9d6477f8686901473
dark <dark.lijin@gmail.com>
2021-12-02 17:42:48 +0800

    fix: support input value is undefined or null

--- packages/components/src/input/index.tsx
+++ packages/components/src/input/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { connect, mapReadPretty } from '@formily/react'
-import { Input as AntdInput, Loading } from 'antd-mobile'
+import { connect, mapProps, mapReadPretty } from '@formily/react'
+import { Input as AntdInput } from 'antd-mobile'
+import { isValid } from '@formily/shared'
 import { InputProps } from 'antd-mobile/es/components/input'
 import { PreviewText } from '../preview-text'
 
-
 export const Input: React.FC<InputProps> = connect(
   AntdInput,
+  mapProps((props) => {
+    return {
+      value: isValid(props.value) ? props.value : '',
+    }
+  }),
   mapReadPretty(PreviewText.Input)
 )
 
