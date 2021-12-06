d8fc7218343bb8e1beab40f2f1e37f841539512f
dark <dark.lijin@gmail.com>
2021-12-06 11:28:47 +0800

    docs: site show brandName

--- packages/components/.umirc.js
+++ packages/components/.umirc.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 export default {
   mode: 'site',
   logo: '//img.alicdn.com/imgextra/i2/O1CN01Kq3OHU1fph6LGqjIz_!!6000000004056-55-tps-1141-150.svg',
-  title: 'Formily',
+  title: 'Antd Mobile',
   hash: true,
   favicon:
     '//img.alicdn.com/imgextra/i3/O1CN01XtT3Tv1Wd1b5hNVKy_!!6000000002810-55-tps-360-360.svg',
@@ -62,10 +62,11 @@ export default {
       padding-right: 58px!important;
     }
     .__dumi-default-navbar-logo{
-      height: 60px !important;
-      width: 150px !important;
-      padding-left:0 !important;
-      color: transparent !important;
+      background-size: 140px!important;
+      background-position: center left!important;
+      background-repeat: no-repeat!important;
+      padding-left: 150px!important;/*可根据title的宽度调整*/
+      font-size: 18px!important;
     }
     .__dumi-default-navbar{
       padding: 0 28px !important;
