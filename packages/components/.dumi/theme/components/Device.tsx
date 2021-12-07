5c6c9c33759d8b303bf863317dd7a6b665c03ba6
dark <dark.lijin@gmail.com>
2021-12-07 15:13:49 +0800

    docs: update site mobile device preview

--- packages/components/.dumi/theme/components/Device.tsx
+++ packages/components/.dumi/theme/components/Device.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useContext, useEffect, useRef } from 'react'
+import type { FC } from 'react'
 import QRCode from 'qrcode.react'
 import { context, usePrefersColor } from 'dumi/theme'
+import Spin from './Spin'
 import { ROUTE_MSG_TYPE } from 'dumi-theme-mobile/es/layouts/demo'
 import './Device.less'
-import type { FC } from 'react'
 
 interface IDeviceProps {
   className?: string
@@ -14,11 +15,12 @@ const Device: FC<IDeviceProps> = ({ url, className }) => {
   const iframeRef = useRef<HTMLIFrameElement>()
   const [iframeSrc, setIframeSrc] = useState<string>()
   const [renderKey, setRenderKey] = useState(Math.random())
+  const [loading, setLoading] = useState(true)
   const [color] = usePrefersColor()
   const {
     config: { mode, theme },
   } = useContext(context)
-  const carrier = theme?.carrier || 'dumi'
+  // const carrier = theme?.carrier || 'dumi'
 
   // re-render iframe if prefers color changed
   useEffect(() => {
@@ -47,6 +49,11 @@ const Device: FC<IDeviceProps> = ({ url, className }) => {
     }
   }, [url])
 
+  const onLoad = (e) => {
+    //  Toast.clear()
+    setLoading(false)
+  }
+
   return (
     <div
       className={['__dumi-default-device'].concat(className).join(' ')}
@@ -57,32 +64,40 @@ const Device: FC<IDeviceProps> = ({ url, className }) => {
        <span className="__dumi-default-device-status-carrier">{carrier}</span>
        <span>10:24</span>
        </div>*/}
-      <div className="mobile-header"></div>
-      <div className="mobile-content">
-        <iframe
-          ref={iframeRef}
-          title="dumi-previewer"
-          src={`${iframeSrc}?iframe`}
-          key={renderKey}
-        />
-      </div>
-      <div className="__dumi-default-device-action">
-        <button
-          className="__dumi-default-icon"
-          role="refresh"
-          onClick={() => setRenderKey(Math.random())}
-        />
-        <button className="__dumi-default-icon" role="qrcode">
-          {url && <QRCode value={url} size={96} />}
-        </button>
-        <a
-          href={url}
-          target="_blank"
-          rel="noreferrer"
-          className="__dumi-default-icon"
-          role="open-demo"
-        />
-      </div>
+      <div className="mobile-header" />
+      <Spin spinning={loading} wrapperClassName="wrapper-loading">
+        <div className="mobile-content">
+          <iframe
+            ref={iframeRef}
+            title="dumi-previewer"
+            src={`${iframeSrc}?iframe`}
+            onLoad={onLoad}
+            key={renderKey}
+          />
+          <div className="__dumi-default-device-action">
+            <button
+              className="__dumi-default-icon"
+              role="refresh"
+              onClick={() => {
+                setLoading(true)
+                setTimeout(() => {
+                  setRenderKey(Math.random())
+                })
+              }}
+            />
+            <button className="__dumi-default-icon" role="qrcode">
+              {url && <QRCode value={url} size={96} />}
+            </button>
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="__dumi-default-icon"
+              role="open-demo"
+            />
+          </div>
+        </div>
+      </Spin>
     </div>
   )
 }
