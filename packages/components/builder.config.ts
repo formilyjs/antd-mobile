6fda4f8b9d09f6c1d3de691d11af175b56ae899e
李金 <dark.lijin@gmail.com>
2022-11-02 16:42:21 +0800

    chore: update builder.config

--- packages/components/builder.config.ts
+++ packages/components/builder.config.ts
@@ -4,4 +4,7 @@ export const BuilderConfig: IBuilderConfig = {
   targetLibName: 'antd-mobile',
   targetLibCjsDir: 'lib',
   targetLibEsDir: 'es',
+  externals: {
+    'antd-mobile': 'antdMobile',
+  },
 }
